ec70aaa4dc00513c8e7f3234a0efee94cb997a00
Umigishi-Aoi <umigishi.aoi@gmail.com>
2024-09-19 11:18:42 +0900

    make plain ui

--- lib/presentation/suggestion_page.dart
+++ lib/presentation/suggestion_page.dart
@@ -6,7 +6,35 @@ class SuggestionPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            const Text('地名を入力してください。'),
+            SizedBox(
+              width: 320,
+              child: TextField(
+                decoration: InputDecoration(
+                  hintText: '東京',
+                  hintStyle: TextStyle(color: Theme.of(context).hintColor),
+                ),
+              ),
+            ),
+            Container(
+              padding: const EdgeInsets.all(8),
+              width: 320,
+              height: 120,
+              decoration: BoxDecoration(
+                border: Border.all(
+                  color: Theme.of(context).primaryColor,
+                ),
+                borderRadius: BorderRadius.circular(8),
+              ),
+              child: const Text('ラーメン'),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
